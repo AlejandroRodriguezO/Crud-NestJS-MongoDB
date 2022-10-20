d4e716b0a4417571c967fbd442e0387cbc8fb624
Jorge Alejandro Rodriguez Oviedo <alejoo9415@gmail.com>
2022-10-20 11:43:34 -0500

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { AccountModule } from './account/account.module';
 import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [AccountModule, MongooseModule.forRoot('mongodb+srv://dbTest:R8RU6f9mwnuabnkI@nestjs.8woatdq.mongodb.net/test')],
+  imports: [AccountModule, MongooseModule.forRoot(ENV.ROOT)],
   controllers: [AppController],
   providers: [AppService],
 })
